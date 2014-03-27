bc31c7d06c540d7c3480171a38f14cba2ad23a14
Taylor Ren <taylor.ren@gmail.com>
2014-03-27 19:35:31 +0800

    Finalized the test PHP script.

--- complex/test/testComplex.php
+++ complex/test/testComplex.php
@@ -4,11 +4,19 @@ $c=new Complex(-3,-4);
 echo $c->mod()."\n";
 
 $d=new Complex(4,3);
-$e=$d->add($c);
+$e=$d->add($c)->add($c);
 
 echo ($d->mod()."\n");
 echo ((string)$d."\n");
        
-var_dump($e);
-
 echo ($e->mod()."\n");
+
+$f=new \DateTime();
+try
+{
+    $g=$d->add($f); //Error!
+}
+catch (Exception $e)
+{
+    echo "Something horrible happened.";
+}
